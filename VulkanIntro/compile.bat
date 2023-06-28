d4421e58572610835dd858bfcabeb668611f532a
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2023-06-29 00:25:54 +0100

    messing with vert shader

--- VulkanIntro/compile.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\RyanB\Downloads\install\install\bin\glslc.exe shader.vert -o vert.spv
-C:\Users\RyanB\Downloads\install\install\bin\glslc.exe shader.frag -o frag.spv
-pause
---
+++ VulkanIntro/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:\Users\RyanB\install\bin\glslc.exe shader.vert -o vert.spv
+C:\Users\RyanB\install\bin\glslc.exe shader.frag -o frag.spv
+pause
