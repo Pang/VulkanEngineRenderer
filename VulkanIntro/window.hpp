fcb35e9b9d9160b13e0cac9ca542ef2f5d06806e
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2023-06-30 19:49:01 +0100

    viewport info fix

--- VulkanIntro/window.hpp
+++ VulkanIntro/window.hpp
@@ -15,8 +15,10 @@ namespace graphicsEngine {
 		Window& operator=(const Window&) = delete;
 
 
-		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 		bool shouldClose() { return glfwWindowShouldClose(glfwWindow); }
+		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
+
+		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 
 	private:
 		void initWindow();
